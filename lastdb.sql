8670909416511a3ce9c61b856913096ac51487aa
YoanhClarky <manteleyoanh@gmail.com>
2023-11-06 20:37:27 +0100

    ajout db

--- lastdb.sql
+++ lastdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : sam. 14 oct. 2023 à 08:27
+-- Généré le : lun. 06 nov. 2023 à 20:36
 -- Version du serveur : 10.4.22-MariaDB
 -- Version de PHP : 8.1.1
 
@@ -20,6 +20,33 @@ SET time_zone = "+00:00";
 --
 -- Base de données : `mono`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `actualites`
+--
+
+CREATE TABLE `actualites` (
+  `id` int(11) NOT NULL,
+  `titre` varchar(255) NOT NULL,
+  `ddate` varchar(255) NOT NULL,
+  `img` varchar(255) DEFAULT NULL,
+  `contenu` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `active` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `actualites`
+--
+
+INSERT INTO `actualites` (`id`, `titre`, `ddate`, `img`, `contenu`, `created_at`, `updated_at`, `active`) VALUES
+(1, 'Nouvelle version du smartphone XYZ-12 bientôt disponible', '15 Octobre 2023', '0', 'L\'entreprise ABC Tech a annoncé que la prochaine version de son smartphone phare, le XYZ-12, sera bientôt disponible sur le marché. Cette mise à jour apporte de nouvelles fonctionnalités, une meilleure autonomie de la batterie et un design amélioré.', '2023-10-20 17:24:45', '2023-10-20 17:24:45', 1),
+(2, 'Lancement d\'une application de réalité augmentée révolutionnaire', '18 Octobre 2023', '0', 'Une start-up innovante, TechVisAR, a dévoilé sa nouvelle application de réalité augmentée qui promet de révolutionner la manière dont nous interagissons avec le monde numérique. Cette application offre des expériences immersives inégalées, allant de la formation professionnelle à la navigation urbaine.', '2023-10-20 17:24:45', '2023-10-20 17:24:45', 1),
+(3, 'Découverte d\'une faille de sécurité majeure dans les systèmes informatiques', '20 Octobre 2023', '0', 'Des chercheurs en sécurité ont identifié une faille de sécurité majeure dans les systèmes informatiques largement utilisés. Les experts travaillent actuellement sur un correctif pour résoudre ce problème potentiellement grave qui pourrait affecter des millions d\'utilisateurs.', '2023-10-20 17:24:45', '2023-10-20 17:24:45', 1),
+(4, 'Une vie motivée par l\'éssentiel', '12 Octobre 2023', 'C:\\Users\\mante\\AppData\\Local\\Temp\\php2F51.tmp', 'L\'entreprise ABC Tech a annoncé que la prochaine version de son smartphone phare, le XYZ-12, sera bientôt disponible sur le marché. Cette mise à jour apporte de nouvelles fonctionnalités, une meilleure autonomie de la batterie et un design amélioré.', '2023-10-20 17:15:23', '2023-10-20 17:15:23', 1);
 
 -- --------------------------------------------------------
 
@@ -59,6 +86,31 @@ INSERT INTO `annees` (`id`, `designation`, `etat`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `commentaires`
+--
+
+CREATE TABLE `commentaires` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `contenu` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `actif` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `commentaires`
+--
+
+INSERT INTO `commentaires` (`id`, `nom`, `contenu`, `created_at`, `updated_at`, `actif`) VALUES
+(1, 'Mantele Yoanh', 'Lalalalala', '2023-10-25 21:06:05', '2023-10-25 21:06:05', 1),
+(2, 'Mabiala Chris', 'Je mange', '2023-10-25 21:44:48', '2023-10-25 21:44:48', 0),
+(3, 'Mantele Yoanh', 'Pour styliser les bordures et ajouter une ombre légère et fantaisiste à chaque élément du carrousel, vous pouvez utiliser des classes CSS Bootstrap pour contrôler ces aspects. Voici comment vous pouvez le faire', '2023-10-25 21:59:24', '2023-10-25 21:59:24', 0),
+(4, 'CHRISTIAN MANTELE', 'jdbnjnrovr', '2023-10-28 19:45:29', '2023-10-28 19:45:29', 1);
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `courcycles`
 --
 
@@ -80,18 +132,20 @@ CREATE TABLE `courcycles` (
 --
 
 INSERT INTO `courcycles` (`id`, `type_id`, `cour_id`, `cycle_id`, `created_at`, `updated_at`, `token`, `url_pdf`, `etat`, `yeux`) VALUES
-(1, 2, 1, 1, '2023-09-25 15:08:29', '2023-10-02 19:35:12', 'jbgfidzbfik', 'pdfs/1691486397.pdf', 1, 0),
-(2, 1, 1, 2, '2023-09-25 15:08:29', '2023-09-25 15:08:29', 'jkdbvkjd', 'pdfs/Methode 5S.pdf', 1, 1),
+(2, 1, 1, 2, '2023-09-25 15:08:29', '2023-10-17 21:24:27', 'jkdbvkjd', 'pdfs/Methode 5S.pdf', 1, 1),
 (3, 1, 1, 3, '2023-09-25 15:08:29', '2023-09-29 07:21:00', 'kjvkhv', 'pdfs/1691486397.pdf', 1, 1),
-(4, 1, 2, 1, '2023-09-25 15:08:29', '2023-09-25 15:08:29', 'bdkvbdjvl', 'pdfs/MONOGRAPHIE1.pdf', 1, 1),
-(5, 2, 3, 1, '2023-09-25 15:08:29', '2023-09-25 15:08:29', 'jdkvkjdqbv', 'pdfs/MONOGRAPHIE1.pdf', 1, 1),
+(4, 1, 2, 1, '2023-09-25 15:08:29', '2023-10-18 13:02:07', 'bdkvbdjvl', 'pdfs/MONOGRAPHIE1.pdf', 1, 1),
+(5, 2, 3, 1, '2023-09-25 15:08:29', '2023-10-18 13:02:08', 'jdkvkjdqbv', 'pdfs/MONOGRAPHIE1.pdf', 1, 1),
 (6, 1, 2, 2, '2023-09-25 15:08:29', '2023-09-25 15:08:29', 'kjdsbvoidqjbv', 'pdfs/1691486397.pdf', 1, 1),
 (7, 1, 4, 2, '2023-09-26 06:14:30', '2023-09-26 06:14:30', 'ihfipehfpie', 'pdfs/MONOGRAPHIE1.pdf', 1, 1),
 (8, 1, 5, 3, '2023-09-29 17:44:05', '2023-09-29 17:44:05', NULL, NULL, 1, 1),
 (9, 1, 6, 1, '2023-09-29 17:50:32', '2023-09-29 17:50:32', NULL, NULL, 1, 1),
 (10, 1, 7, 1, '2023-10-02 19:34:20', '2023-10-02 19:34:20', NULL, NULL, 1, 1),
 (11, 1, 8, 1, '2023-10-06 11:45:34', '2023-10-06 11:45:34', 'dlbfozbg', NULL, 1, 1),
-(12, 2, 9, 1, '2023-10-06 11:52:39', '2023-10-06 17:03:55', '0df2dbd827e7dae810127ec3864b5f94e7f2e163', NULL, 1, 1);
+(12, 2, 9, 1, '2023-10-06 11:52:39', '2023-10-06 17:03:55', '0df2dbd827e7dae810127ec3864b5f94e7f2e163', NULL, 1, 1),
+(13, 2, 10, 1, '2023-10-18 05:17:43', '2023-10-18 05:17:43', 'ba2679cefae7365ae42144ae5075213747849bad', NULL, 1, 1),
+(16, 1, 19, 3, '2023-10-18 07:08:02', '2023-10-18 07:08:02', '69a8817f6235b8a95d620cbdf43126b6c43f3563', 'courpdf/1697616482.pdf', 1, 1),
+(17, 1, 20, 1, '2023-10-18 07:12:08', '2023-10-18 07:12:08', '1e15d36830c8f440a775220e2bf8d1a3e97bd9e2', 'courpdf/1697616728.pdf', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -155,7 +209,10 @@ INSERT INTO `cours` (`id`, `designation`, `description`, `created_at`, `updated_
 (6, 'Visual basic', 'Programmation web', '2023-09-29 17:50:32', '2023-09-29 17:50:32', '8df006698419c342f9e5b0dc9db457241ba906e8', 1, 1),
 (7, 'Mathematiques', 'Calculs', '2023-10-02 19:34:20', '2023-10-02 19:34:20', 'c693ca269e4c6ab806cf7e84481dcdb4c10fd394', 1, 1),
 (8, 'Anglais', 'Parler anglais', '2023-10-06 11:45:34', '2023-10-06 11:45:34', '411cf23b13f088d890561cddc0c70d91cbf7c7c5', 1, 1),
-(9, 'Anglais', 'Parler mieux l\'anglais', '2023-10-06 11:52:39', '2023-10-06 11:52:39', '1fcc1907460e0c0d0aaca1129e5d06fa679ad352', 1, 1);
+(9, 'Anglais', 'Parler mieux l\'anglais', '2023-10-06 11:52:39', '2023-10-06 11:52:39', '1fcc1907460e0c0d0aaca1129e5d06fa679ad352', 1, 1),
+(10, 'Mathematiques', 'Calcule', '2023-10-18 05:17:43', '2023-10-18 05:17:43', '87b8e72d0006fdedd2d0c0d7e1bf2a1707c575f3', 1, 1),
+(19, '.NET', 'Developpement web C#', '2023-10-18 07:08:02', '2023-10-18 07:08:02', '69a8817f6235b8a95d620cbdf43126b6c43f3563', 1, 1),
+(20, 'EPS', 'atrrtrttuuyryu', '2023-10-18 07:12:08', '2023-10-18 07:12:08', '1e15d36830c8f440a775220e2bf8d1a3e97bd9e2', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -299,7 +356,7 @@ CREATE TABLE `livres` (
 --
 
 INSERT INTO `livres` (`id`, `auteur`, `titre`, `soustitre`, `edition`, `lieupub`, `maisoned`, `datepub`, `page`, `created_at`, `updated_at`, `url_pdf`, `token`, `etat`, `yeux`) VALUES
-(1, 'Yoanh', 'Manger', 'Faim', 'sjblfljdqs', 'kdlnvldnv', 'dlkbvmosdjVB', '2023-09-01', 200, '2023-09-25 00:09:15', '2023-09-29 06:18:18', 'pdfs/1691486397.pdf', 'IHFG%MQOIRBVOIREBVQ%IV', 1, 1),
+(1, 'Yoanh', 'Manger du pain', 'Avec du lait', 'premiere', 'Boutique', 'Pointe-Noire', '2023-10-18', 100, '2023-09-25 00:09:15', '2023-10-18 20:20:17', 'pdfs/1691486397.pdf', 'IHFG%MQOIRBVOIREBVQ%IV', 1, 1),
 (2, 'dhmosh', 'djbvkvmjdbq', 'djqbfkdqjbkq', 'duhfoiuf', 'djbodljv', 'jdbsgldv', '2023-09-02', 149, '2023-09-25 00:09:15', '2023-09-29 06:51:49', 'pdfs/1691486397.pdf', 'OZOHZOUBVZOIBVZOVZBOVOIBVIBVAPIBOAVEPOIBVA', 1, 1),
 (3, NULL, 'Une vie motivée par l\'éssentiel', 'OURANIA', 'RICK WARREN', NULL, NULL, NULL, 399, '2023-09-27 11:46:24', '2023-09-27 11:46:24', NULL, NULL, 1, 1),
 (4, 'MANTELE YOANH', 'Reflechissez et devenez riche', 'pourquoi devenir riche ?', 'Arcrane', 'Congo', 'Yahoo', '2023-09-05', 288, '2023-09-27 11:50:56', '2023-09-27 11:50:56', NULL, NULL, 1, 1),
@@ -314,7 +371,7 @@ INSERT INTO `livres` (`id`, `auteur`, `titre`, `soustitre`, `edition`, `lieupub`
 (13, 'MANTELE YOANH', 'Se lever', 'pourquoi devenir riche ?', 'RICK WARREN', 'Congo', 'Yahoo', '2023-09-12', 34, '2023-09-27 22:59:20', '2023-09-27 22:59:20', 'photolivre/6c80dcf6d0da927b404b23c9d5f79a469e26a5a2.pdf', NULL, 1, 1),
 (14, 'MAKAYA', 'Reflechissez et devenez riche', 'Pourquoi suis-je sur terre ?', 'RICK WARREN', NULL, NULL, NULL, 12, '2023-09-27 23:27:22', '2023-09-27 23:50:03', 'livrephoto/1695860842.pdf', NULL, 1, 1),
 (15, 'MANTELE YOANH', 'Dormir debout', 'Pourquoi suis-je sur terre', 'Navda', 'New York', 'Yahoo', '2023-09-05', 244, '2023-09-28 05:20:02', '2023-09-28 05:20:02', 'livrephoto/1695882002.pdf', NULL, 1, 1),
-(16, 'MAKAYA', 'Aller au travail', 'pourquoi devenir riche ?', 'Arcane', 'New York', 'Yahoo', '2023-09-04', 300, '2023-09-29 06:25:28', '2023-09-29 06:26:06', 'livrephoto/1695972328.pdf', 'e626bc09c9ae204f1e768bd16dac11f4d0003be6', 1, 1),
+(16, 'MAKAYA', 'Aller au travails', 'pourquoi devenir riche ?', 'Arcane', 'New York', 'Yahoo', '2023-09-04', 300, '2023-09-29 06:25:28', '2023-10-18 20:17:25', 'livrephoto/1695972328.pdf', 'e626bc09c9ae204f1e768bd16dac11f4d0003be6', 1, 1),
 (17, 'Mabiala', 'Aller travailler', 'pourquoi devenir riche', 'New edition', 'Chicago', 'Google', '2023-09-01', 200, '2023-09-29 06:34:11', '2023-09-29 06:34:11', 'livrephoto/1695972851.pdf', 'a9e292095bb97435097f034eeada8bf7e0151185', 1, 1),
 (18, 'Mabiala', 'Aller travailler encore', 'pourquoi devenir riche', 'New edition', 'Chicago', 'Google', '2023-09-01', 200, '2023-09-29 06:39:54', '2023-09-29 06:51:52', 'livrephoto/1695973194.pdf', '24ad7174d05a0788db2d3d5be60ee6b8fe7eb9d2', 1, 1),
 (19, 'Mabiala', 'Aller travailler encore plus', 'pourquoi devenir riche', 'New editions tp', 'Chicago', 'Google', '2023-09-01', 200, '2023-09-29 16:41:04', '2023-10-02 19:28:34', 'livrephoto/1696010730.pdf', 'e5dce6db2d6b29f7c1552fd1c7ad76e9b90e01e9', 1, 1),
@@ -406,10 +463,11 @@ CREATE TABLE `resumes` (
 --
 
 INSERT INTO `resumes` (`id`, `designation`, `cour_id`, `url_pdf`, `created_at`, `updated_at`, `token`, `etat`, `yeux`) VALUES
-(1, 'resume de la methodologie', 3, 'pdfs/1695551156.pdf', '2023-09-25 13:48:00', '2023-10-06 17:48:14', '2udo', 1, 1),
-(2, 'resumé du developpement web', 2, 'pdfs/1695551156.pdf', '2023-09-25 13:48:00', '2023-09-25 13:48:00', '7695GGDOH', 1, 1),
-(3, 'resumer des maths', 1, 'pdfs/1695551156.pdf', '2023-09-25 13:48:00', '2023-09-25 13:48:00', 'GUOO2689203', 1, 1),
-(4, 'resume de cisco', 5, 'resumepdf/1696618632.pdf', '2023-10-06 17:57:12', '2023-10-06 18:31:20', 'c37a8c84ec797b2add4d0f77acc3c5ad92f9e7b1', 1, 1);
+(1, 'resume de la methodologie', 3, 'pdfs/1695551156.pdf', '2023-09-25 13:48:00', '2023-10-17 20:52:19', '2udo', 1, 1),
+(2, 'resumé du developpement web api', 2, 'pdfs/1695551156.pdf', '2023-09-25 13:48:00', '2023-10-18 19:53:14', '7695GGDOH', 1, 1),
+(3, 'resumer des maths', 1, 'pdfs/1695551156.pdf', '2023-09-25 13:48:00', '2023-10-18 13:06:48', 'GUOO2689203', 1, 1),
+(4, 'resume de cisco', 5, 'resumepdf/1696618632.pdf', '2023-10-06 17:57:12', '2023-10-18 13:10:46', 'c37a8c84ec797b2add4d0f77acc3c5ad92f9e7b1', 1, 1),
+(6, 'Resume eps la redemption', 20, 'resumepdf/1697638087.pdf', '2023-10-18 13:08:07', '2023-10-18 19:55:42', 'effdc67955bf42ce275afc5ce16bbae28520de10', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -429,7 +487,7 @@ CREATE TABLE `tels` (
 --
 
 INSERT INTO `tels` (`id`, `nbr`, `created_at`, `updated_at`) VALUES
-(1, 76, '2023-10-06 19:13:37', '2023-10-06 18:13:37');
+(1, 83, '2023-10-28 20:47:57', '2023-10-28 19:47:57');
 
 -- --------------------------------------------------------
 
@@ -468,24 +526,37 @@ CREATE TABLE `users` (
   `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `role` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Déchargement des données de la table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'Mantele', 'mantele@gmail.com', NULL, '$2y$10$8rHZCXGJ6ikx4Ki17UtLZOhJvBkaUUI5ha9r.CUWXTbZ72JJpkBUu', NULL, '2023-07-28 20:54:17', '2023-07-28 20:54:17');
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`, `role`) VALUES
+(1, 'Mantele', 'mantele@gmail.com', NULL, '$2y$10$8rHZCXGJ6ikx4Ki17UtLZOhJvBkaUUI5ha9r.CUWXTbZ72JJpkBUu', NULL, '2023-07-28 20:54:17', '2023-07-28 20:54:17', 1);
 
 --
 -- Index pour les tables déchargées
 --
 
 --
+-- Index pour la table `actualites`
+--
+ALTER TABLE `actualites`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Index pour la table `annees`
 --
 ALTER TABLE `annees`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `commentaires`
+--
+ALTER TABLE `commentaires`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -593,16 +664,28 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT pour la table `actualites`
+--
+ALTER TABLE `actualites`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT pour la table `annees`
 --
 ALTER TABLE `annees`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
+-- AUTO_INCREMENT pour la table `commentaires`
+--
+ALTER TABLE `commentaires`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT pour la table `courcycles`
 --
 ALTER TABLE `courcycles`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT pour la table `courfilieres`
@@ -614,7 +697,7 @@ ALTER TABLE `courfilieres`
 -- AUTO_INCREMENT pour la table `cours`
 --
 ALTER TABLE `cours`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT pour la table `cycles`
@@ -668,7 +751,7 @@ ALTER TABLE `personnes`
 -- AUTO_INCREMENT pour la table `resumes`
 --
 ALTER TABLE `resumes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT pour la table `tels`
